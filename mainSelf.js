18c5fe80cfb4c12207fad1752ed43199f07a059c
eddie wang <wang8119@gmail.com>
2023-09-14 12:20:16 +0800

    視圖更新

--- mainSelf.js
+++ mainSelf.js
@@ -14,15 +14,62 @@ import {Dep, effectWatch, reactive} from'./core/index.js'
 // // a.notice()
 
 
-reactive
-const user = reactive({
-  age:10
-}) 
-let nextAge = 0
+// //reactive
+// const user = reactive({
+//   age:10
+// }) 
+// let nextAge = 0
 
-effectWatch(() => {
-  nextAge = user.age + 1
-  console.log(nextAge)
-})
+// effectWatch(() => {
+//   nextAge = user.age + 1
+//   console.log(nextAge)
+// })
 
-user.age++
+// user.age++
+
+// const context = reactive({
+//   count:0
+// }) 
+
+// window.context = context
+
+// effectWatch(() => {
+//   // // ui邏輯
+//   document.querySelector("#app").textContent = ""
+//   const element = document.createElement("div")
+//   const text = document.createTextNode("nihao")
+//   const text1 = document.createTextNode(context.count)
+//   element.append(text)
+//   element.append(text1)
+//   document.querySelector("#app").append(element)
+// })
+
+// 改成更像vue組件
+const App = {
+  //template -> render
+  render(context){
+    effectWatch(() => {  //收集依賴
+      // ui邏輯
+      document.querySelector("#app").textContent = ""
+      const element = document.createElement("div")
+      const text = document.createTextNode("nihao")
+      const text1 = document.createTextNode(context.count) //收集依賴
+      element.append(text)
+      element.append(text1)
+      document.querySelector("#app").append(element)
+    })
+  },
+
+  // 處理數據
+  setup(){
+    const obj = reactive({
+      count:0
+    }) 
+    window.obj = obj
+    return {
+      obj
+    }
+  }
+}
+
+App.render(App.setup())
